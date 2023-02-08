a1fbd58168b416d955c1b23b9b4117069e519329
kharioki <tony.kharioki@gmail.com>
2023-02-08 23:59:54 +0300

    fetch supabase data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,17 @@
+import { useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase";
+
+export const loader = async () => {
+  const { data, error } = await supabase.from("messages").select("*");
+  return { data, error };
+};
+
 export default function Index() {
+  const {data} = useLoaderData();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Lets do this 🦉!!!</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
